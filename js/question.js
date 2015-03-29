a86cef689d5c19a59168fae8c62e05afa4f436c5
Slavik <slavka.com.ua@gmail.com>
2015-03-29 06:12:45 +0300

    v0.5.0 изменена структура question.js - теперь с прототипами, блоки не пересоздаются заново

--- js/question.js
+++ js/question.js
@@ -1,66 +1,63 @@
 /*global $ */
 /*global currentQuestion*/
 
-var body = $(document.body)
-
 function Answer (text, nextQuestion, result, id) {
-  return {
-    textAnswer: text,
-    result: result == undefined ? null : result,
-    nextQuestion: ParseQuestions(nextQuestion),
-    show: function(answerBlock) {
-      answerBlock.addEl(
-        $.newEl('label')
-          .addClass('answer-question')
-          .setAttr('data-id', id)
-          .addEl(
-            $.newEl('input')
-              .setAttr('type', 'radio')
-              .setAttr('name')
-          ).addEl(
-            $.newEl('span')
-              .text(this.textAnswer)
-          )
+  this.textAnswer = text
+  this.result = result == undefined ? null : result
+  this.nextQuestion = ParseQuestions(nextQuestion)
+  this.id = id
+}
+
+Answer.prototype.show = function(answerBlock) {
+  answerBlock.addEl(
+    $.newEl('label')
+      .addClass('answer-question')
+      .setAttr('data-id', this.id)
+      .addEl(
+        $.newEl('input')
+          .setAttr('type', 'radio')
+          .setAttr('name')
+      ).addEl(
+        $.newEl('span')
+          .text(this.textAnswer)
       )
-    },
-    showResult: function(block) {
-      block.clear()
-      block.addEl($.newEl('div').addClass('question-result').text(this.result))
-    }
-  }
+  )
+}
+
+Answer.prototype.showResult = function(block) {
+  block.clear()
+  block.addEl($.newEl('div').addClass('question-result').text(this.result))
 }
 
 function Question (text, answers) {
-  return {
-    questionBlock: null,
-    textQuestion: text,
-    answersQuestion: answers,
-    show: function() {
-      this.questionBlock = this.questionBlock || body.$('.question-block')
-      if (this.questionBlock == null) {
-        this.questionBlock = $.newEl('div').addClass('question-block')
-        body.addEl(this.questionBlock)
-      }
-      this.questionBlock.clear()
-      this.questionBlock.addEl($.newEl('div').addClass('question-text').text(this.textQuestion))
-      var questionAnswers = $.newEl('div').addClass('question-answers').clear(),
-        questionButton = $.newEl('input').setAttr('type', 'button').setAttr('value', 'Ответить').addClass('question-button')
-      for (var i = 0; i < this.answersQuestion.length; i++) {
-        this.answersQuestion[i].show(questionAnswers)
-      }
-      this.questionBlock.addEl(questionAnswers).addEl(questionButton)
-      questionButton.addEventListener('click', this.clickAnswerButton.bind(this), false)
-    },
-    clickAnswerButton: function() {
-      var id = Number($('input:checked').parent().getAttr('data-id'))
-      if (this.answersQuestion[id].result == null) {
-        currentQuestion = this.answersQuestion[id].nextQuestion
-        currentQuestion.show()
-      }
-      else {
-        this.answersQuestion[id].showResult(this.questionBlock)
-      }
-    }
+  this.textQuestion = text
+  this.answersQuestion = answers
+}
+
+Question.prototype.questionBlock = $('.question-block')
+Question.prototype.questionText = $('.question-text')
+Question.prototype.questionAnswers = $('.question-answers')
+Question.prototype.questionButton = $('.question-button')
+
+Question.prototype.show = function() {
+  this.questionText.text(this.textQuestion)
+  this.questionAnswers.clear()
+  for (var i = 0; i < this.answersQuestion.length; i++) {
+    this.answersQuestion[i].show(this.questionAnswers)
+  }
+  this.currentFunction__ClickAnswerButton = this.clickAnswerButton.bind(this)
+  this.questionButton.addEventListener('click', this.currentFunction__ClickAnswerButton, false)
+}
+
+Question.prototype.clickAnswerButton = function() {
+  this.questionButton.removeEventListener('click', this.currentFunction__ClickAnswerButton, false)
+  var id = Number($('input:checked').parent().getAttr('data-id'))
+  if (this.answersQuestion[id].result == null) {
+    currentQuestion = this.answersQuestion[id].nextQuestion
+    currentQuestion.show()
+  }
+  else {
+    this.answersQuestion[id].showResult(this.questionBlock)
   }
 }
 
