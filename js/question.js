4bbddd9c922b3a0c116fe2d0a2f5ba1897fb6cae
Slavik <slavka.com.ua@gmail.com>
2015-03-29 07:04:59 +0300

    v0.6.2 добавил "неактивность у боковых кнопок" добавил предка у вопросов

--- js/question.js
+++ js/question.js
@@ -1,10 +1,10 @@
 /*global $ */
-/*global currentQuestion*/
+/*global questions*/
 
-function Answer (text, nextQuestion, result, id) {
+function Answer (text, nextQuestion, result, id, previousQuestion) {
   this.textAnswer = text
   this.result = result == undefined ? null : result
-  this.nextQuestion = ParseQuestions(nextQuestion)
+  this.nextQuestion = ParseQuestions(nextQuestion, previousQuestion)
   this.id = id
 }
 
@@ -35,6 +35,8 @@ Answer.prototype.showResult = function(block) {
 function Question (text, answers) {
   this.textQuestion = text
   this.answersQuestion = answers
+  this.answerId = 0
+  this.previousQuestion = null
 }
 
 Question.prototype.questionBlock = $('.question-block')
@@ -48,7 +50,7 @@ Question.prototype.show = function() {
   for (var i = 0; i < this.answersQuestion.length; i++) {
     this.answersQuestion[i].show(this.questionAnswers)
   }
-  this.questionAnswers.$('label input').checked = true
+  this.questionAnswers.$('label.answer-question[data-id="' + this.answerId + '"] input').checked = true
   this.currentFunctionClickAnswerButton = this.clickAnswerButton.bind(this)
   this.questionButton.addEventListener('click', this.currentFunctionClickAnswerButton, false)
 }
@@ -57,22 +59,26 @@ Question.prototype.clickAnswerButton = function() {
   this.questionButton.removeEventListener('click', this.currentFunctionClickAnswerButton, false)
   var id = Number($('input:checked').parent().getAttr('data-id'))
   if (this.answersQuestion[id].result == null) {
-    currentQuestion = this.answersQuestion[id].nextQuestion
-    currentQuestion.show()
+    this.answersQuestion[id].nextQuestion.show()
   }
   else {
     this.answersQuestion[id].showResult(this.questionBlock)
   }
 }
 
-function ParseQuestions (questions) {
+function ParseQuestions (questions, previousQuestion) {
   if (questions == null) {
     return null
   }
-  return new Question(
-    questions.textQuestion,
-    questions.answersQuestion.map(function (answer, id) {
-      return new Answer(answer.textAnswer, answer.nextQuestion, answer.result, id)
-    })
-  )
+  var result = new Question(questions.textQuestion)
+  result.answersQuestion = questions.answersQuestion.map(function (answer, id) {
+      return new Answer(answer.textAnswer, answer.nextQuestion, answer.result, id, this)
+    }, result)
+  result.previousQuestion = previousQuestion
+  return result
 }
+
+var c = ParseQuestions(questions[Math.trunc(Math.random() * questions.length)])
+
+//ParseQuestions(questions[Math.trunc(Math.random() * questions.length)]).show()
+c.show()
--- js/script.js
+++
@@ -1,7 +0,0 @@
-/*global ParseQuestions */
-/*global questions */
-/*global Math */
-
-var currentQuestion = ParseQuestions(questions[Math.trunc(Math.random() * questions.length)])
-
-currentQuestion.show()
